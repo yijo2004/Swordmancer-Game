4deca1c21f97f8edf3f47bd14bd87932501b285c
mydoot <buaqur@gmail.com>
2026-02-02 21:24:00 -0800

    Added an enemy template

--- Scenes/Enemies/enemy.gd
+++ Scenes/Enemies/enemy.gd
@@ -1,9 +1,29 @@
 extends CharacterBody2D
 
+var speed = 25
+var chasePlayer = false
+var player = null
 
+func _physics_process(delta: float) -> void:
+	if chasePlayer:
+		if position.distance_to(player.position) > 10:
+				position+=(player.position-position)/speed
+				
+				if (player.position.x-position.x)<0:
+					$Sprite2D.flip_h = true
+				else:
+					$Sprite2D.flip_h = false
+	
+	move_and_collide(Vector2(0,0))
+	
+	
+	
 func _on_detection_area_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
+	player = body
+	chasePlayer = true
+	
 
 
 func _on_detection_area_body_exited(body: Node2D) -> void:
-	pass # Replace with function body.
+	player = null
+	chasePlayer = false
