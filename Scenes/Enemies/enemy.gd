2f0f8181f8d0d6e64814279aae31f5144577ccd7
mydoot <buaqur@gmail.com>
2026-02-02 20:58:48 -0800

    Added Enemy scene and script

---
+++ Scenes/Enemies/enemy.gd
@@ -0,0 +1,9 @@
+extends CharacterBody2D
+
+
+func _on_detection_area_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
+
+
+func _on_detection_area_body_exited(body: Node2D) -> void:
+	pass # Replace with function body.
