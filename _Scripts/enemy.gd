7a2157f8f8c5d6c8122be8b539cb98a2a4985182
mydoot <buaqur@gmail.com>
2026-02-05 22:05:05 -0800

    Work on enemy.gd

--- _Scripts/enemy.gd
+++ _Scripts/enemy.gd
@@ -25,8 +25,6 @@ func _on_detection_area_body_entered(body: Node2D) -> void:
 	player = body
 	chasePlayer = true
 	
-
-
 func _on_detection_area_body_exited(body: Node2D) -> void:
 	if body == player:
 		player = null
